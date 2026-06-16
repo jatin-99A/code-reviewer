d8e4c9bc7387d6cbb1fd92d74f51ed7e4ec135d9
jatin <rudraking714@gmail.com>
2026-06-17 01:11:02 +0530

    chore: setup Prisma with Neon DB and configure environment variables

---
+++ lib/db.ts
@@ -0,0 +1,45 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import env from "@/app/config/env";
+
+const connectionString = `${env.DATABASE_URL}`;
+
+// helper for create client
+const createClient = () => {
+    const adapter = new PrismaPg({ connectionString });
+    return new PrismaClient({ adapter });
+};
+
+let cached = (globalThis as any).prisma;
+
+if (!cached) {
+    cached = (globalThis as any).prisma = { instance: null, promise: null };
+}
+
+async function db() {
+
+    if (process.env.NODE_ENV === "production") {
+        return createClient();
+    }
+
+    if (cached.instance) {
+        return cached.instance;
+    }
+
+    if (!cached.promise) {
+        cached.promise = Promise.resolve().then(() => {
+            return createClient();
+        });
+    }
+
+    try {
+        cached.instance = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.instance;
+}
+
+export default db;
---
+++ prisma/migrations/20260616191741_test/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "checking" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "checking_pkey" PRIMARY KEY ("id")
+);
